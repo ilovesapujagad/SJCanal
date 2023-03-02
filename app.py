9b49c3e4f28f902028e75461bccc5e0c5934d1f5
lovesapujagad <118962746+ilovesapujagad@users.noreply.github.com>
2023-03-02 20:33:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -90,6 +90,35 @@ def connector():
     except Exception as e:
         print(e)
         return jsonify({'status':'error'}),403
+    
+@app.get("/connector/<connector>")
+def connectorbyid(connector):
+    try:
+        url = "http://10.10.65.5:8083/connectors"
+        response = requests.get(url)
+        x = len(response.json())
+        liststatus = []
+        listsconfig = []
+
+        
+        url = "http://10.10.65.5:8083/connectors/"+ connector +"/status"
+        response1 = requests.get(url)
+        liststatus.append(response1.json())
+
+        url = "http://10.10.65.5:8083/connectors/"+ connector +"/config"
+        response1 = requests.get(url)
+        listsconfig.append(response1.json())
+        dicti={}
+        dicti["response"] = {"config":listsconfig[0]},{"status":liststatus[0]}
+
+        
+        return jsonify(dicti)
+
+    
+    except Exception as e:
+        print(e)
+        return jsonify({'status':'error'}),403
+
 
 
 
